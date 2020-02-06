74ca3096f9b1a7d3364df7581e5f2f3197a8af0d
ednei <edneifneto@gmail.com>
2020-02-06 13:16:47 -0300

    add line main

--- app/src/main/java/com/example/milano/ui/MainActivity.kt
+++ app/src/main/java/com/example/milano/ui/MainActivity.kt
@@ -42,7 +42,6 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_toolbar, menu)
         return true
